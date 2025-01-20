f15b0a1049994aab2b5fb657cb0dab78f97c8d84
Ayush Kumar Yadav <ayushyadav8056@gmail.com>
2025-01-20 23:34:01 +0530

    Added code changes

--- src/SOLID/Main.java
+++ src/SOLID/Main.java
@@ -3,5 +3,6 @@ package SOLID;
 public class Main {
     public static void main(String[] args) {
         CreditCard cc1 = new Amex(new SameWalletRefund());
+        System.out.println("Amex card");
     }
 }
